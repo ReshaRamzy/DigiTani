07ad186261aa302c4e0a0b6440884ae4d6e14f70
rahayurdn <93781028+rahayurdn@users.noreply.github.com>
2023-06-05 15:29:20 +0700

    Update config.js

--- Cloud-Computing/config.js
+++ Cloud-Computing/config.js
@@ -1,7 +1,7 @@
 // config.js
 module.exports = {
     PORT: process.env.PORT || 3000, // Port number for the server
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://rahayurdn:digitani123@digitani.bexnk7d.mongodb.net/?retryWrites=true&w=majority', // MongoDB connection URI
-    JWT_SECRET: process.env.JWT_SECRET || 'nyangkultani', // Secret key for JWT authentication
+    MONGODB_URI: process.env.MONGODB_URI || '', // MongoDB connection URI
+    JWT_SECRET: process.env.JWT_SECRET || '', // Secret key for JWT authentication
   };
   
