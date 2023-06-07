9303509dbc300441c7a770b980fd0983c2281f2a
Zidantfnno21 <zidantifano@gmail.com>
2023-06-08 02:51:14 +0700

    Update to Motion Layout

--- Mobile-Development/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ Mobile-Development/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState : Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContentView(R.layout.activity_main)
 
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
