a82d2c60ac0743af0fd6d997ea170b32e3d17793
Zidantfnno21 <zidantifano@gmail.com>
2023-06-13 06:27:17 +0700

    Bottom Navigation

--- Machine-Learning/recom-container/main.py
+++ Machine-Learning/recom-container/main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from flask import Flask, jsonify, request
 import tensorflow as tf
 from tensorflow.python.lib.io import file_io
@@ -28,4 +29,36 @@ def predict():
     return jsonify({'predicted_class': predicted_class})
 
 if __name__ == '__main__':
+=======
+from flask import Flask, jsonify, request
+import tensorflow as tf
+from tensorflow.python.lib.io import file_io
+import numpy as np
+import os
+from sklearn.preprocessing import LabelEncoder
+
+app = Flask(__name__)
+
+def load_model():
+    model_path = 'gs://digitani_model_recom/rec.h5'
+
+    with file_io.FileIO(model_path, mode='rb') as model_file:
+        model = tf.keras.models.load_model(model_file)
+    
+    return model
+
+model = load_model()
+label_encoder = LabelEncoder()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    input_data = np.array(data['input_data']).reshape(1, -1)
+    predictions = model.predict(input_data)
+    predicted_class_encoded = np.argmax(predictions)
+    predicted_class = label_encoder.inverse_transform([predicted_class_encoded])[0]
+    return jsonify({'predicted_class': predicted_class})
+
+if __name__ == '__main__':
+>>>>>>> 39e2d129600a8cfb5fbd5102c9430f914b5d66c6
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
