1f7ede8f9044e2614d427388e2de774f2f0201f5
ReshaRamzy <92032258+ReshaRamzy@users.noreply.github.com>
2023-06-15 20:07:42 +0700

    Update config.js

--- Cloud-Computing/config.js
+++ Cloud-Computing/config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-    PORT: process.env.PORT || 3000, // Port number for the server
+    PORT: process.env.PORT || 8080, // Port number for the server
     MONGODB_URI: process.env.MONGODB_URI || '', // MongoDB connection URI
     JWT_SECRET: process.env.JWT_SECRET || '', // Secret key for JWT authentication
   };
