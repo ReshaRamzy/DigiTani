17136d7ad92fedaaae1bccd734fe2c90d03e71b9
ReshaRamzy <92032258+ReshaRamzy@users.noreply.github.com>
2023-06-15 20:07:28 +0700

    Update index.js

--- Cloud-Computing/index.js
+++ Cloud-Computing/index.js
@@ -17,6 +17,6 @@ app.use('/api/user', userRoutes);
 app.use('/api/predict', predictRoutes);
 
 // Start the server
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
