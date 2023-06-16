d52e9faf4e0955db632b4f3ccb4735760836a3ba
rahayurdn <93781028+rahayurdn@users.noreply.github.com>
2023-06-16 20:36:18 +0700

    Update Disease.js

--- Cloud-Computing/database/models/Disease.js
+++ Cloud-Computing/database/models/Disease.js
@@ -1,24 +1,89 @@
 const mongoose = require('mongoose');
 
-const diseaseSchema = new mongoose.Schema({
-  id_disease: {
+// Crops Schema
+const cropsSchema = new mongoose.Schema({
+  id_type_crops: {
     type: Number,
     required: true,
   },
-  name: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  recommended_action: {
+  name_crops: {
     type: String,
     required: true,
   },
 });
 
-const Disease = mongoose.model('Disease', diseaseSchema);
+const Crops = mongoose.model('Crops', cropsSchema);
 
-module.exports = Disease;
+// Crops Disease Schema
+const cropsDiseaseSchema = new mongoose.Schema({
+  id_disease: {
+    type: Number,
+    required: true,
+  },
+  id_type_crops: {
+    type: Number,
+    required: true,
+  },
+  image_disease: {
+    type: String,
+    required: true,
+  },
+  name_disease: {
+    type: String,
+    required: true,
+  },
+  category_disease: {
+    type: String,
+    required: true,
+  },
+  desc_disease: {
+    type: String,
+    required: true,
+  },
+  recAction_disease: {
+    type: String,
+    required: true,
+  },
+  causeOf_disease: {
+    type: String,
+    required: true,
+  },
+});
+
+const CropsDisease = mongoose.model('CropsDisease', cropsDiseaseSchema);
+
+// Crops Recommendation Schema
+const cropsRecommendationSchema = new mongoose.Schema({
+  id_crops: {
+    type: Number,
+    required: true,
+  },
+  name_crops: {
+    type: String,
+    required: true,
+  },
+  image_crops: {
+    type: String,
+    required: true,
+  },
+  time_crops: {
+    type: String,
+    required: true,
+  },
+  alert_crops: {
+    type: String,
+    required: true,
+  },
+  desc_crops: {
+    type: String,
+    required: true,
+  },
+});
+
+const CropsRecommendation = mongoose.model('CropsRecommendation', cropsRecommendationSchema);
+
+module.exports = {
+  Crops,
+  CropsDisease,
+  CropsRecommendation,
+};
